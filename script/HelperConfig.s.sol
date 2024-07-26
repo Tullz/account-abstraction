c15ed69a7d91b14e6bf6d3d4a5003bee29575970
Tully <tully.slattery@gmail.com>
2024-07-26 15:09:27 +0800

    Started MinimalAccountTest suite

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -16,6 +16,7 @@ contract HelperConfig is Script {
     uint256 constant ZKSYNC_SEPOLIA_CHAIN_ID = 300;
     uint256 constant LOCAL_CHAIN_ID = 31337;
     address constant BURNER_WALLET = 0xFfcbAcBF10Ba900D7cEe19a57953994250cB1567;
+    address constant FOUNDRY_DEFAULT_WALLET = 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
 
     NetworkConfig public localNetworkConfig;
     mapping(uint256 chainId => NetworkConfig) public networkConfigs;
@@ -47,10 +48,10 @@ contract HelperConfig is Script {
     }
 
     function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
-        if (networkConfigs[ETH_SEPOLIA_CHAIN_ID].account != address(0)) {
+        if (localNetworkConfig.account != address(0)) {
             return localNetworkConfig;
         }
 
-        // deploy a mock entry point contract
+        return NetworkConfig({entryPoint: address(0), account: FOUNDRY_DEFAULT_WALLET});
     }
 }
